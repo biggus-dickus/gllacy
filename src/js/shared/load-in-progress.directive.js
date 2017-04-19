df678072056922c1a810a515656b1e914c87314f
Max Maslenko <maxm@netvertise.com>
2017-04-19 10:09:54 +0300

    Fixes for spinner directive

--- src/js/shared/load-in-progress.directive.js
+++ src/js/shared/load-in-progress.directive.js
@@ -1,23 +1,22 @@
-(function() {
+(function () {
   'use strict';
 
   angular.module('glApp').directive('loadInProgress', directive);
 
   function directive() {
     return {
-      link: function (scope, elem) {
-        var spinner = angular.element('<div class="loader"></div>');
-
-        console.log(scope.$parent.ctrl.loading);
-        elem.append(spinner);
-
-        scope.$watch(scope.$parent, function() {
-          if (scope.$parent.ctrl.loading === false) {
-            spinner.remove();
+      scope: {
+        showSpinner: '=loadInProgress'
+      },
+      link: function(scope, elem) {
+        scope.$watch('showSpinner', function (showSpinner) {
+          if (showSpinner) {
+            elem.append('<div class="loader"></div>');
+          } else {
+            elem.find('.loader').remove();
           }
         });
       }
     };
   }
-
 })();
